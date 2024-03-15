f6be588a30e54cb15814a54d8b210612d725b5f0
wasif-islam <wasif.islam@g.bracu.ac.bd>
2024-03-16 02:58:16 +0600

    last modifaction to fix errors

---
+++ backend/data/menuItem.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        itemName: 'Rice',
+        itemPrice: 30,
+        calCount: 130,
+    },
+    {
+        itemName: 'Meat',
+        itemPrice: 80,
+        calCount: 145,
+    },
+    {
+        itemName: 'Fish',
+        itemPrice: 75,
+        calCount: 206,
+    }
+     
+]; 
+
+export default products;
